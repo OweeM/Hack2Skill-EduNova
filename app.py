2620d682443e4ac494874365ad1d63fdac06da99
OweeM <oweemirajkar@gmail.com>
2025-04-06 18:51:20 +0530

    Fix: Bind to 0.0.0.0 and dynamic port for Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, render_template, redirect, session
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
@@ -68,4 +69,5 @@ def login():
         return render_template('index.html', message="Invalid email or password")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
